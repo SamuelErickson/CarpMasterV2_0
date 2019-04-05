55e0912fe9a189b24b75c88b0a3d63aeb7dee5de
Sam Erickson PC <eric4204@umn.edu>
2019-04-04 21:34:09 -0500

    Edited testThermo to take data from unlimited thermometers

--- testThermo.py
+++ testThermo.py
@@ -31,7 +31,7 @@ def read_temp(device_folder_list):
         lines = read_temp_raw(file)
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
-            lines = read_temp_raw()
+            lines = read_temp_raw(file)
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
             temp_string = lines[1][equals_pos + 2:]
