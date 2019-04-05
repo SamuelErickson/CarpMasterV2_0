49ba67c3bf9f3c1c669d2f652d072dbcbf4a3a09
Sam Erickson PC <eric4204@umn.edu>
2019-04-04 21:28:55 -0500

    Edited testThermo to take data from unlimited thermometers

--- testThermo.py
+++ testThermo.py
@@ -9,31 +9,38 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+
+device_file_list = glob.glob(base_dir + '28*')
+for folder in device_folder_list:
+    folder = folder + '/w1_slave'
+
+#device_folder = glob.glob(base_dir + '28*')[0]
+#device_file = device_folder + '/w1_slave'
 
 
-def read_temp_raw():
-    f = open(device_file, 'r')
+def read_temp_raw(filename):
+    f = open(filename, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
 
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
+def read_temp(device_folder_list):
+    output = []
+    for file in device_folder_list:
         lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos + 2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos + 2:]
+            temp_c = float(temp_string) / 1000.0
+        output = (output,temp_c)
+    return output
 
-def read_temp_raw():
-    catdata = subprocess.Popen(['cat',device_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def read_temp_raw(filename):
+    catdata = subprocess.Popen(['cat',filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out,err = catdata.communicate()
     out_decode = out.decode('utf-8')
     lines = out_decode.split('\n')
