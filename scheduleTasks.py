4280fd1b56f980efc0480f2b8d55164ffc133baa
Sam Erickson PC <eric4204@umn.edu>
2019-04-05 21:51:52 -0500

    trying new scheduler

--- scheduleTasks.py
+++ scheduleTasks.py
@@ -17,15 +17,18 @@ job1 = cron.new(command=Command1)
 job1.minute.every(1)
 
 # Make web interface reboot upon restart and run as background task
-Command2= 'export FLASK_APP=/home/pi/app.py'
+Command2 = '{ export FLASK_APP=/home/pi/CarpMasterV2_0/app.py; flask run --host=0.0.0.0 & }'
+
+Command4= 'export FLASK_APP=/home/pi/CarpMasterV2_0/app.py'
 Command3 = 'flask run --host=0.0.0.0 &'
+
 
 
 
 job2 = cron.new(command=Command2)
 job2.every_reboot()
-job3 = cron.new(command=Command3)
-job3.every_reboot()
+#job3 = cron.new(command=Command3)
+#job3.every_reboot()
 
 cron.write()
 
