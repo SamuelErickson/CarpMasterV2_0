fc6afc0494cfa2900486e28e6150b1aef34705a6
Sam Erickson PC <eric4204@umn.edu>
2019-04-05 23:03:02 -0500

    added light toggle

--- scheduleTasks.py
+++ scheduleTasks.py
@@ -4,6 +4,7 @@
 
 import pandas as pd
 from crontab import CronTab
+import pandas as pd
 
 #The following line only works in Linux
 #https://stackabuse.com/scheduling-jobs-with-python-crontab/
@@ -17,20 +18,28 @@ Command1= 'sudo python3 /home/pi/CarpMasterV2_0/example1.py'
 job1 = cron.new(command=Command1)
 job1.minute.every(1)
 
-# Make web interface reboot upon restart and run as background task
-Command2 = '{ export FLASK_APP=/home/pi/CarpMasterV2_0/app.py; flask run --host=0.0.0.0 & }'
-
-Command4= 'export FLASK_APP=/home/pi/CarpMasterV2_0/app.py'
-Command3 = 'flask run --host=0.0.0.0 &'
 
 #Schedule light on/off switch
-
-
-
-
+Command2 = "python3 /home/pi/CarpMasterV2_0/toggleLight.py"
 
 job2 = cron.new(command=Command2)
-job2.every_reboot()
+job2.minute.every(1)
+
+
+# Make web interface reboot upon restart and run as background task
+#Command2 = '{ export FLASK_APP=/home/pi/CarpMasterV2_0/app.py; flask run --host=0.0.0.0 & }'
+
+#Command4= 'export FLASK_APP=/home/pi/CarpMasterV2_0/app.py'
+#Command3 = 'flask run --host=0.0.0.0 &'
+
+
+
+
+
+
+
+
+#job2.every_reboot()
 #job3 = cron.new(command=Command3)
 #job3.every_reboot()
 
