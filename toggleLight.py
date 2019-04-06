1838dc0232bbac9916a503f2f0bc4e93b6b84cf1
Sam Erickson PC <eric4204@umn.edu>
2019-04-05 22:53:53 -0500

    added light toggle

--- toggleLight.py
+++ toggleLight.py
@@ -5,10 +5,10 @@ pinNum = 17
 GPIO.setup(pinNum,GPIO.OUT)
 
 
-if GPIO.output(pinNum) == LOW:
+if GPIO.input(pinNum) == LOW:
      print("lights on!")
      GPIO.output(pinNum, GPIO.HIGH)
-elif GPIO.output(pinNum) == HIGH:
+elif GPIO.input(pinNum) == HIGH:
      print("lights off!")
      GPIO.output(pinNum, GPIO.LOW)
 else:
